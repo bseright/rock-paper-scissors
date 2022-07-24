f2cf3207b0bf6771159609fb4157cc1caeb7db91
bseright <britainseright@gmail.com>
2022-07-24 16:38:44 -0500

    create function to play a single round

--- script.js
+++ script.js
@@ -20,8 +20,42 @@
 
 const items = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
+let userScore;
+
+let computerScore;
+
+function getComputerSelection() {
     return items[Math.floor(Math.random() * items.length)];
 }
 
-console.log(getComputerChoice());
+function getUserSelection() {
+    return prompt("Make a selection of rock, paper, or scissors.",).toLowerCase();
+}
+
+function playRound(userSelection, computerSelection) {
+    if (userSelection === computerSelection) {
+        return `Both parties selected ${userSelection}!\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`
+    } else if (userSelection === "rock" && computerSelection === "scissors") {
+        return `You win! The computer selected ${computerSelection}. One point has been added to your score.\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`;
+    } else if (userSelection === "rock" && computerSelection === "paper") {
+        return `You lose! The computer selected ${computerSelection}. One point has been added to the computer's score.\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`;
+    } else if (userSelection === "paper" && computerSelection === "rock") {
+        return `You win! The computer selected ${computerSelection}. One point has been added to your score.\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`;
+    } else if (userSelection === "paper" && computerSelection === "scissors") {
+        return `You lose! The computer selected ${computerSelection}. One point has been added to the computer's score.\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`;
+    } else if (userSelection === "scissors" && computerSelection === "paper") {
+        return `You win! The computer selected ${computerSelection}. One point has been added to your score.\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`;
+    } else if (userSelection === "scissors" && computerSelection === "rock") {
+        return `You lose! The computer selected ${computerSelection}. One point has been added to the computer's score.\n\nUser Score: ${userScore}\nComputer Score: ${computerScore}`;
+    } else {
+        return "Enter an appropriate choice.";
+    }
+}
+
+
+
+const userSelection = getUserSelection();
+
+const computerSelection = getComputerSelection();
+
+console.log(playRound(userSelection, computerSelection));
